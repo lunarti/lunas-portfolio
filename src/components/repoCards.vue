13de9fe1202bd876594397e82aee4ef2de5fefe4
lunarti <lunarti@gmail.com>
2022-04-03 21:35:23 -0300

    axios typescript

---
+++ src/components/repoCards.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="repo">{{ repoName  }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'repoCards',
+  props: {
+      repoName: String,
+      repoUrl: String,
+      reporStarCount: Number,
+      reporDescription: String,
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h1 class="text-6xl m-4">
+      <span class="text-red-500">Victor Lunarti Valadão</span>
+    </h1>
+    
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import axios from "axios";
+import repoCards from "@/components/repoCards.vue"
+
+interface Repo {
+  repoName: string,
+  repoUrl: string,
+  repoStarCount: number,
+  repoDescription: string,
+}
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      repos: [] as Repo[]
+    };
   },
+  mounted(){
+    axios.get('https://api.github.com/users/lunarti/repos?sort=updated').then(response => {
+      response.data.slice(1,10).forEach((element: any) => {
+        this.repos.push({
+          repoName: element.name,
+          repoDescription: element.description,
+          repoStarCount: element.stargazers_count,
+          repoUrl: element.html_url,
+        })
+        
+      });
+    }).catch(console.error)
+      
+    
+    },
+    
 });
 </script>
